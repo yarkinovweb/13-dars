1539a8356322abf023425258a7db6136e9df0ea7
yarkinovweb <yarkinovweb@gmail.com>
2024-04-24 14:51:13 +0500

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+import rasm1 from './bmw.png';
+import rasm2 from './city.png';
+import rasm3 from './nature.png';
+
+const tugma = (event) => event.preventDefault();
+
+const rasmlar = [
+  <img src={rasm1} onDragStart={tugma} role='presentation' />,
+  <img src={rasm2} onDragStart={tugma} role='presentation' />,
+  <img src={rasm3} onDragStart={tugma} role='presentation' />
+];
+
+const Header = () => {
+  return (
+    <AliceCarousel
+      mouseTracking
+      items={rasmlar}
+      activeIndex={0}
+      animationDuration={500}
+      animationType={'slide'}
+      autoPlay={true}
+      autoPlayControls={true}
+      autoPlayDirection={'ltr'}
+      disableButtonsControls={true}
+      disableDotsControls={true}
+      disableSlideInfo={false}
+      infinite={true}
+      responsive={{
+        0: {
+          items: 1,
+        },
+        1024: {
+          items: 3,
+          itemsFit: 'contain',
+        }
+      }}
+    />
+  )
+}
+
+export default Header
